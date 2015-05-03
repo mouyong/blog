d1855d6ce33350c3587a048c7d3b7459ae556dcb
mouyong <my24251325@gmail.com>
2015-05-03 17:55:26 +0800

    Modify website template

--- Public/Js/append.js
+++ Public/Js/append.js
@@ -1,7 +1,7 @@
 $(function(){
-	$('div.first>p>br').remove();
-	$('div.first>p').append('&nbsp;<b>...</b>');
+	$('div.summary>p>br').remove();
+	$('div.summary>p').append('&nbsp;<b>...</b>');
 	//后台添加
-	$('td.first>p:eq(0)>br:eq(0)').remove();
-	$('td.first>p').append('&nbsp;<b>...</b>');
+	$('td.summary>p:eq(0)>br:eq(0)').remove();
+	$('td.summary>p').append('&nbsp;<b>...</b>');
 });
